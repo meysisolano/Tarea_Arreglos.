1b524e20c00af5dea986433ed3e41cf3dd61d229
Meysi Solano <meysi.solano01@gmail.com>
2025-06-20 20:04:34 -0600

    Estudiante.java

--- src/main/java/org/ejercicio2/Estudiante.java
+++ src/main/java/org/ejercicio2/Estudiante.java
@@ -1,4 +1,13 @@
-package main.java.org.ejercicio2;
+package org.ejercicio2;
 
 public class Estudiante {
+    String nombres;
+    String apellidos;
+    double promedio;
+
+    public Estudiante(String nombres, String apellidos, double promedio) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.promedio = promedio;
+    }
 }
